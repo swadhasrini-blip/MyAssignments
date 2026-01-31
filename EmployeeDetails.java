37be63da41799bd1a9c324eab974cac8688b7426
swadhasrini-blip <swadhasrini@gmail.com>
2026-01-31 20:11:50 +0530

    Add files via upload
    day1

---
+++ EmployeeDetails.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+
+		String name = "John Doe";
+		short age = 30;
+		float salary = 55000.75f;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210l;
+		int pinCode = 56001;
+		short luckyNumber = 7;
+
+		System.out.println("name:" + name + "\n" + "age:" + age + "\n" + "salary:" + salary + "\n" + "gender:" + gender
+				+ "\n" + "acceptTerms:" + acceptTerms + "\n" + "rating:" + rating + "\n" + "mobileNumber:"
+				+ mobileNumber + "\n" + "pinCode:" + pinCode + "\n" + "luckyNumber:" + luckyNumber);
+
+	}
+
+}
